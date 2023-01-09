0646ed75434dddcbe22f774d932a7f8b6db2d52c
Prajit7 <71118373+Prajit7@users.noreply.github.com>
2023-01-09 08:53:43 +0530

    Add files via upload

---
+++ AssignmentDotnet/a4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SampleConApp
+{
+    class Utilities1
+    {
+        internal static string Prompt(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        internal static int GetNumber(string question)
+        {
+            return int.Parse(Prompt(question));
+        }
+    }
+    class a4Array
+    {
+        static void dynamicArrayExample()
+        {
+            int size = Utilities1.GetNumber("Enter the Size of the Array");
+
+            string typeName = Utilities1.Prompt("Please enter the CTS Equilavent name for the type of the array that U want to create");
+            Type type = Type.GetType(typeName, true, true);
+            Array myArray = Array.CreateInstance(type, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                string enteredValue = Utilities1.Prompt($"Enter the value of the type {type.Name}");
+                object convertedValue = Convert.ChangeType(enteredValue, type);
+                myArray.SetValue(convertedValue, i);
+            }
+            Console.WriteLine("All the values are set");
+            foreach (object item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            dynamicArrayExample();
+        }
+    }
+}
---
+++ AssignmentDotnet/a6.cs
@@ -0,0 +1,48 @@
+﻿using SampleConApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class a6DateValid
+    {
+        static void Main(string[] args)
+        {
+            int year = Utilities.GetNumber("Enter the year");
+            int month = Utilities.GetNumber("Enter the month");
+            int day = Utilities.GetNumber("Enter the day");
+            if (isValidDate(year, month, day))
+            {
+                Console.WriteLine("Valid");
+            }
+            else Console.WriteLine("Invalid");
+
+
+        }
+        static bool checkLeap(int year)
+        {
+            return ((year % 4 == 0) && (year % 400 == 0));
+        }
+        static bool isValidDate(int year, int month, int day)
+        {
+            if (month > 12 || month < 0 || day < 0 || day > 31) return false;
+
+            if (month == 2)
+            {
+                if (checkLeap(year))
+                {
+                    return (day <= 29);
+                }
+                else return (day <= 28);
+            }
+            if (month == 4 || month == 6 || month == 9 || month == 11) return (day <= 30);
+
+            return true;
+
+
+        }
+    }
+}
